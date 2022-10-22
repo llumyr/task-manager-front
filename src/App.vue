5fee247812d9778dfd582a10e3bfb9aac34f0c06
Vladimir <86754494+dazeGG@users.noreply.github.com>
2022-10-22 11:47:39 +0300

    added color schemes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <router-view/>
+  <app-header/>
+  <div class="container">
+    <router-view/>
+    <button @click="toggleColorScheme">Change color scheme</button>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { onBeforeMount } from 'vue'
 import { useStore } from 'vuex'
+import AppHeader from '@/components/AppHeader.vue'
 
 const store = useStore()
 
 onBeforeMount(async () => {
+  document.documentElement.dataset.colorScheme = 'dark'
+  if (localStorage.colorScheme === 'light') document.documentElement.dataset.colorScheme = 'light'
   await store.dispatch('initAuth')
 })
+
+const toggleColorScheme = (): void => {
+  document.documentElement.dataset.colorScheme = document.documentElement.dataset.colorScheme === 'light' ? 'dark' : 'light'
+  localStorage.colorScheme = document.documentElement.dataset.colorScheme
+}
 </script>
 
 <style lang="scss">
-@import '@/assets/default';
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import '@/assets/style';
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="header">
+    <router-link :to="Routes.home" class="header__logo">Task Manager</router-link>
+  </header>
+</template>
+
+<script setup lang="ts">
+import Routes from '@/router/Routes'
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
+
+const isAuth = computed(() => store.getters.isAuth)
+</script>
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
