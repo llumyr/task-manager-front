4f66adf2ffb3bf2dc4a0d758e2f7c9287234209d
Vladimir <86754494+dazeGG@users.noreply.github.com>
2022-10-24 01:44:20 +0300

    added animation to reg auth forms

--- src/pages/RegAuthPage.vue
+++ src/pages/RegAuthPage.vue
@@ -2,19 +2,25 @@
   <div class="reg-auth">
     <div class="reg-auth__wrapper">
       <div class="reg-auth__item">
-        <div class="content" @click="showReg = !showReg">
+        <div class="content animate__animated" style="text-align: center; cursor: pointer" @click="regToggleForm"
+             v-show="!regShow" ref="regTextElement">
           <h1>Registration</h1>
-          <p v-if="!showReg">If you don`t have an account</p>
+          <p>If you don`t have an account</p>
         </div>
-        <div class="reg-auth__form" v-if="showReg">
+        <div class="reg-auth__form animate__animated" v-show="regShow" ref="regFormElement">
           <input type="text" placeholder="username">
           <input type="password" placeholder="password">
         </div>
       </div>
       <div class="reg-auth__item">
-        <div class="content">
+        <div class="content animate__animated" style="text-align: center; cursor: pointer" @click="authToggleForm"
+             v-show="!authShow" ref="authTextElement">
           <h1>Authorization</h1>
           <p>If you already have an account</p>
+        </div>
+        <div class="reg-auth__form animate__animated" v-show="authShow" ref="authFormElement">
+          <input type="text" placeholder="username">
+          <input type="password" placeholder="password">
         </div>
       </div>
     </div>
@@ -23,8 +29,70 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import 'animate.css'
 
-const showReg = ref(false)
+const toggleClass = (element: HTMLElement, className: string): void => {
+  if (element.classList.contains(className)) element.classList.remove(className)
+  else element.classList.add(className)
+}
+
+const regShow = ref(false)
+const regTextElement = ref(null)
+const regFormElement = ref(null)
+
+const regToggleForm = () => {
+  if (authShow.value) authToggleForm()
+  if (regShow.value) {
+    toggleClass(regFormElement.value, 'animate__bounceOut')
+    setTimeout(() => {
+      regShow.value = !regShow.value
+      toggleClass(regFormElement.value, 'animate__bounceOut')
+      toggleClass(regTextElement.value, 'animate__bounceIn')
+    }, 750)
+    setTimeout(() => {
+      toggleClass(regTextElement.value, 'animate__bounceIn')
+    }, 1500)
+  } else {
+    toggleClass(regTextElement.value, 'animate__bounceOut')
+    setTimeout(() => {
+      regShow.value = !regShow.value
+      toggleClass(regTextElement.value, 'animate__bounceOut')
+      toggleClass(regFormElement.value, 'animate__bounceIn')
+    }, 750)
+    setTimeout(() => {
+      toggleClass(regFormElement.value, 'animate__bounceIn')
+    }, 1500)
+  }
+}
+
+const authShow = ref(false)
+const authTextElement = ref(null)
+const authFormElement = ref(null)
+
+const authToggleForm = () => {
+  if (regShow.value) regToggleForm()
+  if (authShow.value) {
+    toggleClass(authFormElement.value, 'animate__bounceOut')
+    setTimeout(() => {
+      authShow.value = !authShow.value
+      toggleClass(authFormElement.value, 'animate__bounceOut')
+      toggleClass(authTextElement.value, 'animate__bounceIn')
+    }, 750)
+    setTimeout(() => {
+      toggleClass(authTextElement.value, 'animate__bounceIn')
+    }, 1500)
+  } else {
+    toggleClass(authTextElement.value, 'animate__bounceOut')
+    setTimeout(() => {
+      authShow.value = !authShow.value
+      toggleClass(authTextElement.value, 'animate__bounceOut')
+      toggleClass(authFormElement.value, 'animate__bounceIn')
+    }, 750)
+    setTimeout(() => {
+      toggleClass(authFormElement.value, 'animate__bounceIn')
+    }, 1500)
+  }
+}
 </script>
 
 <style scoped lang="scss">
@@ -57,7 +125,7 @@ const showReg = ref(false)
       transition: 200ms;
 
       &:hover {
-        transform: scale(1.1);
+        transform: scale(1.05);
       }
     }
   }
