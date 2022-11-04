1e15d8cb8c312899f5cd7e5c4462bfd9a5b5ed9e
Vladimir <86754494+dazeGG@users.noreply.github.com>
2022-11-05 00:16:19 +0300

    added buttons to edit and delete group

--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -25,7 +25,22 @@
     <div class="tasks">
       <div class="tasks__wrapper">
         <div class="tasks__header">
-          <h3 class="tasks__title">{{ chosenGroup.title }}</h3>
+          <div class="tasks__chosenGroup chosenGroup">
+            <h3 class="chosenGroup__title">{{ chosenGroup.title }}</h3>
+            <div class="chosenGroup__actions">
+              <button class="chosenGroup__edit">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
+                  <path fill="#2c3e50" fill-rule="evenodd" d="M21.45 5.42c.2.29.16.68-.1.94l-9.18 9.2a.96.96 0 0 1-.34.19l-3.83 1a.75.75 0 0 1-.92-.92l1-3.83a.78.78 0 0 1 .16-.3l9.23-9.23a.75.75 0 0 1 1.06 0l2.83 2.83a.4.4 0 0 1 .1.12Zm-1.68.4L18 4.07l-8.52 8.52-.62 2.4 2.39-.63 8.52-8.52Z" clip-rule="evenodd"/>
+                  <path fill="#2c3e50" d="M19.64 17.16c.27-2.34.36-4.7.26-7.04a.4.4 0 0 1 .12-.3l.98-.98a.2.2 0 0 1 .34.12c.19 2.8.12 5.6-.2 8.37a4.39 4.39 0 0 1-3.88 3.84c-3.47.38-7.05.38-10.52 0a4.39 4.39 0 0 1-3.87-3.84 45.92 45.92 0 0 1 0-10.66 4.39 4.39 0 0 1 3.87-3.84c2.63-.3 5.33-.36 8-.21a.2.2 0 0 1 .12.34l-1 1a.4.4 0 0 1-.3.1c-2.22-.07-4.46.02-6.65.26a2.89 2.89 0 0 0-2.55 2.52c-.4 3.43-.4 6.9 0 10.32a2.89 2.89 0 0 0 2.55 2.52c3.35.37 6.83.37 10.18 0a2.89 2.89 0 0 0 2.55-2.52Z"/>
+                </svg>
+              </button>
+              <button class="chosenGroup__delete">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none">
+                  <path fill="#ff7777" d="M8.44 4.31h-.19c.1 0 .19-.08.19-.18v.18h7.12v-.18c0 .1.09.18.19.18h-.19V6h1.69V4.12c0-.82-.67-1.5-1.5-1.5h-7.5c-.83 0-1.5.68-1.5 1.5V6h1.69V4.31ZM20.25 6H3.75a.75.75 0 0 0-.75.75v.75c0 .1.08.19.19.19H4.6l.58 12.26c.04.8.7 1.43 1.5 1.43h10.64c.8 0 1.46-.63 1.5-1.43l.58-12.26h1.41A.2.2 0 0 0 21 7.5v-.75a.75.75 0 0 0-.75-.75Zm-3.11 13.69H6.86l-.57-12h11.42l-.57 12Z"/>
+                </svg>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -220,6 +235,22 @@ onBeforeMount(async () => {
 .tasks {
   &__wrapper {
     padding-left: 15px;
+  }
+}
+
+.chosenGroup {
+  display: flex;
+  gap: 8px;
+
+  &:hover {
+    .chosenGroup__actions {
+      display: flex;
+    }
+  }
+
+  &__actions {
+    display: none;
+    gap: 2px;
   }
 }
 </style>
